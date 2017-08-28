ae66b36e94ae871121b8bc71e6d7aae6e1efe647
Hom24 <hom-0824@hotmail.com>
2017-08-28 00:49:52 -0500

    Vistas
    creación de objetos para las vistas, con integración.

--- views/site/finalizar.php
+++ views/site/finalizar.php
@@ -10,13 +10,29 @@ use yii\helpers\Url;
 
 $this->title = 'Finalizar';
 ?>
-<div class="site-about">
-    <p>
-        Hello world!!
-    </p>
-	<div class="panel panel-default">
-		<div class="panel-body">
-			<h3>Actividades</h3>
+<div class="main-health">
+	<div class="fin__main-content">
+		<div class="fin__content-percent text-center">
+			<div class="percent">
+				100%
+			</div>
+		</div>
+		<div class="form-group text-center">
+			<button class="btn btn-default btn-raised">
+				Guardar
+			</button>
+		</div>
+		<p>Enviar el reporte por correo electrónico.</p>
+		<div class="form-group label-floating">
+			<label class="control-label" for="email">Correo electrónico</label>
+			  <div class="input-group">
+					<input type="text" id="email" class="form-control">
+					<span class="input-group-btn">
+						<button type="button" class="btn btn-fab btn-fab-mini">
+							<i class="material-icons">send</i>
+						</button>
+					</span>
+			  </div>
 		</div>
 	</div>
 </div>
--- views/site/listitem.php
+++ views/site/listitem.php
@@ -10,13 +10,99 @@ use yii\helpers\Url;
 
 $this->title = 'Lista de Actividades';
 ?>
-<div class="site-about">
-    <p>
-        Hello world!!
-    </p>
-	<div class="panel panel-default">
-		<div class="panel-body">
-			<h3>Actividades</h3>
+<div class="main-health">
+	<div class="row act__content-title">
+		<h2 class="main-title">Actividades</h2>
+	</div>
+	<div class="row act__main-content">
+		<div class="act__content-btn-additem">
+			<button onclick="window.location.href='<?php echo Url::toRoute(['site/formitem']); ?>'" class="btn btn-success btn-fab">
+				<i class="material-icons">&#xE145;</i>
+			</button>
+		</div>
+		<div class="act__content-list">
+			<ul class="act__list">
+				<li>
+					<div class="clearfix">
+						<div class="pull-left">
+							<button class="btn btn-info btn-fab">
+								1
+							</button>
+						</div>
+						<div class="pull-left list-item-desc">
+							<h3 class="rev__title">Actividad</h3>
+						</div>
+						<div class="pull-right list-item-actions">
+							<button class="btn btn-info btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE150;</i>
+							</button>
+							<button class="btn btn-danger btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE14C;</i>
+							</button>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="clearfix">
+						<div class="pull-left">
+							<button class="btn btn-info btn-fab">
+								2
+							</button>
+						</div>
+						<div class="pull-left list-item-desc">
+							<h3 class="rev__title">Actividad</h3>
+						</div>
+						<div class="pull-right list-item-actions">
+							<button class="btn btn-info btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE150;</i>
+							</button>
+							<button class="btn btn-danger btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE14C;</i>
+							</button>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="clearfix">
+						<div class="pull-left">
+							<button class="btn btn-info btn-fab">
+								3
+							</button>
+						</div>
+						<div class="pull-left list-item-desc">
+							<h3 class="rev__title">Actividad</h3>
+						</div>
+						<div class="pull-right list-item-actions">
+							<button class="btn btn-info btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE150;</i>
+							</button>
+							<button class="btn btn-danger btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE14C;</i>
+							</button>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="clearfix">
+						<div class="pull-left">
+							<button class="btn btn-info btn-fab">
+								4
+							</button>
+						</div>
+						<div class="pull-left list-item-desc">
+							<h3 class="rev__title">Actividad</h3>
+						</div>
+						<div class="pull-right list-item-actions">
+							<button class="btn btn-info btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE150;</i>
+							</button>
+							<button class="btn btn-danger btn-fab btn-fab-mini">
+								<i class="material-icons">&#xE14C;</i>
+							</button>
+						</div>
+					</div>
+				</li>
+			</ul>
 		</div>
 	</div>
 </div>
